d7d63cd5a148d4b3406098e92dc970ab5c7b6669
Andrea Di Fabio <adifabio@amazon.com>
2023-08-22 18:34:03 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/isv-ambassador-program/s3-bucket/aws"
+  version = "2.8.0"
 }
-
